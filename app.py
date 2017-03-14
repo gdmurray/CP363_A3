2dbf51aecdf4ed5997574ebd1af4d1450e9a7a60
Greg Murray <gd-murray@hotmail.com>
2017-03-14 00:45:23 -0400

    First Day of Work

---
+++ app.py
@@ -0,0 +1,187 @@
+from flask import Flask, render_template, request, session, redirect, jsonify
+import datetime, math
+from functions import *
+from functools import wraps
+
+app = Flask(__name__)
+app.secret_key = "CP363"
+
+def login_required(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		try:
+			if session["username"] is not None:
+				return f(*args, **kwargs)
+		except:
+			return redirect("/login")
+	return decorated_function
+
+
+@app.route("/")
+@login_required
+def index():
+	result = my_albums(session["username"])
+	return render_template("index.html", result=result)
+
+@app.route("/login", methods=['POST','GET'])
+def login():
+	if request.method == "GET":
+		return render_template("login.html", error=None)
+	elif request.method == "POST":
+		error = None
+		username = request.form["username"]
+		password = request.form["password"]
+
+		#check if user exists
+		if not user_exists(username):
+			error = "User Does not Exist"
+			return render_template("login.html", error=error)
+
+		#check if right password
+		if not check_password(username, password):
+			error = "Incorrect Password"
+			return render_template("login.html", error=error)
+
+		if error is None:
+			session["username"] = username
+			session["cart"] = []
+			return redirect("/")
+
+
+@app.route("/register", methods=['POST','GET'])
+def register():
+	if request.method == "GET":
+		return render_template("register.html", error=None)
+	elif request.method == "POST":
+		#Create User
+		username = request.form["username"]
+		password = request.form["password"]
+		address = request.form["address"]
+		phone = request.form["phone"]
+		user_id = get_user_id()
+		create_user(user_id, username, password, address, phone)
+
+		#Add Payments
+		card_no = request.form["card"]
+		exp_mo = int(request.form["mm"])
+		exp_yr = int('20' + request.form['yy'])
+		exp_date = datetime.datetime(exp_yr, exp_mo, 1)
+		cv = int(request.form["cv"])
+		c_id = get_credit_id()
+		create_credit(user_id, c_id, card_no, exp_date, cv)
+
+		session["username"] = username
+		session["cart"] = []
+		return redirect("/")
+
+
+@app.route("/search", methods=['POST','GET'])
+@login_required
+def search():
+	if request.method == "POST":
+		search_str = request.form["search_str"]
+		result = search_database(search_str)
+		for res in result:
+			print(res)
+		return render_template("result.html", result=result)
+
+	elif request.method == "GET":
+		return render_template("result.html", result=None)
+
+@app.route("/getCart", methods=['GET'])
+def getCart():
+	result = []
+	try:
+		cart = session["cart"]
+		for item in cart:
+			info = album_info(item)
+			result.append(info)
+		return jsonify(result)
+	except:
+		return (jsonify([]))
+	
+
+@app.route("/logout", methods=['POST', 'GET'])
+def logout():
+	session.pop("username", None)
+	session.pop("cart", None)
+	return redirect("/login")
+
+@app.route("/addCart/<int:album_id>", methods=['POST'])
+def addToCart(album_id):
+	cart = session["cart"]
+	if album_id not in cart:
+		user_id = id_from_username(session["username"])
+		if not user_owns(user_id, album_id):
+			cart.append(album_id)
+			session["cart"] = cart
+			return jsonify({"status": 'Put in Cart'})
+		else:
+			return jsonify({"status": "already Owned"})
+	else:
+		return jsonify({"status": "already in cart"})
+
+@app.route("/removeCart/<int:album_id>", methods=["POST"])
+def removeFromCart(album_id):
+	cart = session["cart"]
+	if album_id in cart:
+		cart.remove(album_id)
+		session["cart"] = cart
+		return jsonify({"status": 'Removed from Cart'})
+	else:
+		return jsonify({"status": "Not in cart"})
+
+@app.route("/newCard", methods=['POST'])
+def newCard():
+	user_id = id_from_username(session["username"])
+	card_no = request.form["card"]
+	exp_mo = int(request.form["mm"])
+	exp_yr = int('20' + request.form['yy'])
+	exp_date = datetime.datetime(exp_yr, exp_mo, 1)
+	cv = int(request.form["cv"])
+	c_id = get_credit_id()
+	create_credit(user_id, c_id, card_no, exp_date, cv)
+	return redirect("/checkout")
+
+@app.route("/getSongs/<int:album_id>")
+def getSongs(album_id):
+	conn, cursor = connection()
+	cursor.execute("""SELECT Songs.song_title from songs WHERE Songs.album_id = %s""", album_id)
+	result = cursor.fetchall()
+	cursor.close(); conn.close()
+	return jsonify(result)
+
+@app.route("/checkout", methods=['POST','GET'])
+def payment():
+	username = session["username"]
+	cards = get_cards(username)
+	result = []
+	cart = session["cart"]
+	for item in cart:
+		info = album_info(item)
+		result.append(info)
+	if request.method == "GET":
+		return render_template("payment.html",cards=cards, cart=result)
+
+	elif request.method == "POST":
+		cno = request.form["cno"]
+		if cno == '' or cno == None or cno == '':
+			return render_template("payment.html",cards=cards, cart=result)
+		else:
+			purchase(username, cart, cno)
+			session["cart"] = []
+			return redirect("/")
+
+@app.route("/all", strict_slashes=False)
+@app.route("/all/<int:page>", strict_slashes=False)
+def seeAll(page=None):
+	result, count = get_all_albums(page)
+	pages = math.ceil(count / 15)
+	if page is None:
+		page = 1
+	print(len(result))
+	return render_template("/all.html",result=result, pages=pages)
+if __name__ == "__main__":
+	app.run(port=8000, debug=True)
+
+
---
+++ functions.py
@@ -0,0 +1,202 @@
+import pymysql as mysql
+import datetime
+
+def connection():
+    conn = mysql.connect(user='root',
+                       password='root',
+                       host='localhost',
+                       database='Notown')
+    cursor = conn.cursor()
+    return conn, cursor
+
+def user_exists(username):
+    conn, cursor = connection()
+    cursor.execute("""SELECT * FROM users
+                        WHERE username = %s""",username)
+    exists = cursor.fetchone()
+    cursor.close(); conn.close()
+    if not exists:
+        return False
+    else:
+        return True
+
+def user_owns(user_id, album_id):
+    conn, cursor = connection()
+    cursor.execute("""SELECT * FROM purchases
+                        WHERE user_id = %s AND album_id = %s""",(user_id, album_id))
+    exists = cursor.fetchone()
+    cursor.close(); conn.close()
+    if not exists:
+        return False
+    else:
+        return True
+
+def check_password(username, password):
+    conn, cursor = connection()
+    cursor.execute("""SELECT * FROM users
+                        WHERE username = %s AND password = %s""", (username, password))
+    correct_password = cursor.fetchone()
+    if not correct_password:
+        return False
+    else:
+        return True
+
+def get_receipt_id():
+    conn, cursor = connection()
+    cursor.execute("""SELECT COUNT(*) FROM receipt""")
+    result = cursor.fetchone()
+    cnt = int(result[0])
+
+    cursor.close()
+    conn.close()
+    return cnt + 1
+
+def get_user_id():
+    conn, cursor = connection()
+    cursor.execute("""SELECT COUNT(*) FROM users""")
+    result = cursor.fetchone()
+    cnt = int(result[0])
+
+    cursor.close()
+    conn.close()
+    return cnt + 1
+
+def get_credit_id():
+    conn, cursor = connection()
+    cursor.execute("""SELECT COUNT(*) FROM cards""")
+    result = cursor.fetchone()
+    cnt = int(result[0])
+
+    cursor.close(); conn.close()
+    return cnt + 1
+
+def get_all_albums(page):
+    if not page:
+        page = 1
+    
+    conn, cursor = connection()
+    cursor.execute("""SELECT COUNT(*) from Albums""")
+    count = cursor.fetchone(); count = int(count[0])
+    start = (15 * (page - 1)); end = (15 * page)
+    cursor.execute("""SELECT Albums.album_title, Albums.price, Musicians.name, Albums.album_id
+                        FROM (Albums 
+                            INNER JOIN Musicians
+                                ON Albums.producer_id=Musicians.sin)
+                        ORDER BY Albums.album_title ASC
+                        LIMIT %s, %s""",(start, end))
+    result = cursor.fetchall()
+    
+    cursor.close(); conn.close()
+    return result, count
+
+def id_from_username(username):
+    conn, cursor = connection()
+    cursor.execute("""SELECT user_id FROM users WHERE username = %s""", username)
+    result = cursor.fetchone()
+    cursor.close(); conn.close()
+    return int(result[0])
+
+def create_user(user_id, username, password, address, phone_number):
+    conn, cursor = connection()
+    cursor.execute("""INSERT INTO users (user_id, username, password, address, phone_number)
+                        VALUES (%s, %s, %s,%s, %s) """,(user_id, username, password, address, phone_number))
+    conn.commit(); cursor.close(); conn.close()
+    return
+
+def get_cards(username):
+    conn, cursor = connection()
+    cursor.execute("""SELECT cards.cc_id, cards.cc_number
+                        FROM cards INNER JOIN users
+                            ON cards.user_id=users.user_id
+                        WHERE users.username = %s""", username)
+    result = cursor.fetchall()
+    lst = []
+    for item in result:
+        lst.append( [item[0], str('****' * 3) + item[1][-4:]] )
+    cursor.close(); conn.close()
+    return lst
+
+def purchase(username, cart, cno):
+    conn, cursor = connection()
+    user_id = id_from_username(username)
+    total = 0.00
+    for album_id in cart:
+        try:
+            cursor.execute("""INSERT INTO purchases (user_id, album_id)
+                                VALUES (%s, %s)""",(user_id, album_id))
+            info = album_info(album_id)
+            total += info["price"]
+        except:
+            print("Already Owned")
+    total = total * 1.13
+    recept_id = get_receipt_id()
+    if total != 0.00:
+        cursor.execute("""INSERT INTO receipt (r_id, price, user_id, purchased, cc_id)
+                            VALUES (%s, %s, %s, %s, %s)""",(recept_id, total, user_id, datetime.datetime.now(), cno))
+        conn.commit() 
+    cursor.close(); conn.close()
+    return
+
+def create_credit(user_id, card_id, card_no, expiry, cv):
+    conn, cursor = connection()
+    cursor.execute("""INSERT INTO cards (cc_id, cc_number, expiry, security_code, user_id)
+                        VALUES (%s, %s, %s, %s, %s)""", (card_id, card_no, expiry, cv, user_id))
+    conn.commit(); cursor.close(); conn.close()
+    return
+
+def my_albums(username):
+    conn, cursor = connection()
+    cursor.execute("""SELECT Albums.album_title, Albums.price, Musicians.name, Albums.album_id 
+FROM purchases
+    INNER JOIN users
+        ON purchases.user_id=users.user_id
+    INNER JOIN albums
+        ON purchases.album_id=albums.album_id
+    INNER JOIN Musicians
+        ON albums.producer_id=Musicians.sin
+    WHERE users.username = %s """, username)
+    result = cursor.fetchall()
+    cursor.close(); conn.close()
+    return result
+
+def album_info(album_id):
+    conn, cursor = connection()
+    cursor.execute("""SELECT Albums.album_title, Albums.price, Musicians.name, Albums.album_id
+                        FROM Albums 
+                        INNER JOIN Musicians
+                            ON Albums.producer_id=Musicians.sin
+                        WHERE album_id = %s """, album_id)
+    d = {}
+    result = cursor.fetchone()
+    d["title"] = result[0]
+    d["price"] = float(result[1])
+    d["artist"] = result[2]
+    d["id"] = result[3]
+    cursor.close(); conn.close()
+    return d
+
+def search_database(search_term):
+    conn, cursor = connection()
+    cursor.execute("""SELECT DISTINCT Albums.album_title, Albums.price, producer.name, Albums.album_id
+                        FROM Perform
+                            INNER JOIN Musicians
+                                ON Perform.musician_id=Musicians.sin
+                            INNER JOIN Albums
+                                ON Perform.album_id=Albums.album_id
+                            INNER JOIN Songs
+                                ON Perform.song_id=Songs.song_id
+                            LEFT JOIN Musicians as producer
+                                ON Albums.producer_id=producer.sin
+                            WHERE
+                                Musicians.name = %s
+                                OR producer.name = %s
+                                OR album_title = %s
+                                OR song_title = %s
+        """, (search_term, search_term, search_term, search_term) )
+
+    result = cursor.fetchall()
+    cursor.close(); conn.close()
+    return result
+
+
+
